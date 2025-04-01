5cf16387e5feeda98ae432dfc2342bf5a4d6a453
govinda <govindarana707@gmail.com>
2025-04-01 17:20:47 +0545

    didsplay function established

--- task1/database.php
+++ task1/database.php
@@ -4,9 +4,9 @@ $username="root";
 $pass="";
 $db="BCA";
 $conn=mysqli_connect($host,$username,$pass,$db);
-if(!$conn){
-    die("Error").mysqli_error($conn);
-}
-echo"Database connected";
+// if(!$conn){
+//     die("Error").mysqli_error($conn);
+// }
+// echo"Database connected";
 
 ?>
--- task1/display.php
+++ task1/display.php
@@ -1,3 +1,16 @@
 <?php
 include('database.php');
+
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Id: " . $row["id"] . " - Name: " . $row['name'] . " - Email: " . $row["email"] . " - Phone Number: " . $row["phone"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn); // Close the database connection
 ?>
