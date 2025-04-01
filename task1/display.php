f329c23a6a8e2858f0f9bb73347073d27c0238cb
govinda <govindarana707@gmail.com>
2025-04-01 17:25:55 +0545

    DIsplay data in table

--- task1/display.php
+++ task1/display.php
@@ -6,7 +6,30 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "Id: " . $row["id"] . " - Name: " . $row['name'] . " - Email: " . $row["email"] . " - Phone Number: " . $row["phone"] . "<br>";
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <table>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile Number</th>
+                <tr>
+                    <td><?php echo$row["id"]?></td>
+                    <td><?php echo$row["name"]?></td>
+                    <td><?php echo$row["email"]?></td>
+                    <td><?php echo$row["phone"]?></td>
+                </tr>
+            </table>
+        </body>
+        </html>
+<?php
     }
 } else {
     echo "0 results";
